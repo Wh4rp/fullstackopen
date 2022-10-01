7a26cb3c21d25d54f924437435d456d75beb2e60
warp <matiasfernandeztaipe@gmail.com>
2022-10-01 19:37:11 -0300

    ex 1.11*

--- part1/ex 1.6-1.14/src/App.js
+++ part1/ex 1.6-1.14/src/App.js
@@ -5,7 +5,10 @@ const Button = ({ handleClick, text}) => (
 )
 
 const StatisticLine = ({ text, value }) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({ good, neutral, bad }) => {
