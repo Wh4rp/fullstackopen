43f5d61c26e390cce50ac9b0b3badff54b00990a
warp <matiasfernandeztaipe@gmail.com>
2022-10-01 19:30:03 -0300

    ex 1.8

--- part1/ex 1.6-1.14/src/App.js
+++ part1/ex 1.6-1.14/src/App.js
@@ -8,12 +8,7 @@ const ShowStat = ({ stat, value }) => (
   <p>{stat} {value}</p>
 )
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+const Statistics = ({ good, neutral, bad }) => {
   function total() {
     return good + neutral + bad
   }
@@ -28,10 +23,6 @@ const App = () => {
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="good"/>
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
-      <Button handleClick={() => setBad(bad + 1)} text="bad"/>
       <h2>statics</h2>
       <ShowStat stat="good" value={good}/>
       <ShowStat stat="neutral" value={neutral}/>
@@ -39,6 +30,23 @@ const App = () => {
       <ShowStat stat="total" value={total()}/>
       <ShowStat stat="average" value={average()}/>
       <ShowStat stat="positive" value={positive()}/>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good"/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text="bad"/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
