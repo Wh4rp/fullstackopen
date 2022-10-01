b4bae6cab934b8b0d88a0129902e1ff0564ab28f
warp <matiasfernandeztaipe@gmail.com>
2022-10-01 19:38:32 -0300

    ex 1.11*

--- part1/ex 1.6-1.14/src/App.js
+++ part1/ex 1.6-1.14/src/App.js
@@ -31,12 +31,14 @@ const Statistics = ({ good, neutral, bad }) => {
     return (
       <div>
         <h2>statics</h2>
-        <StatisticLine text="good" value={good}/>
-        <StatisticLine text="neutral" value={neutral}/>
-        <StatisticLine text="bad" value={bad}/>
-        <StatisticLine text="total" value={total()}/>
-        <StatisticLine text="average" value={average()}/>
-        <StatisticLine text="positive" value={positive()}/>
+        <table>
+          <StatisticLine text="good" value={good}/>
+          <StatisticLine text="neutral" value={neutral}/>
+          <StatisticLine text="bad" value={bad}/>
+          <StatisticLine text="total" value={total()}/>
+          <StatisticLine text="average" value={average()}/>
+          <StatisticLine text="positive" value={positive()}/>
+        </table>
       </div>
     )
   }
