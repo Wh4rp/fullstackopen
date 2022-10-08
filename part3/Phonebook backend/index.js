904cac56336f7c362810fa4cf585e42c8ed129f1
warp <matiasfernandeztaipe@gmail.com>
2022-10-08 13:19:50 -0300

    3.2: Phonebook backend step2

--- part3/Phonebook backend/index.js
+++ part3/Phonebook backend/index.js
@@ -28,7 +28,12 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} person</p><p>${new Date()}</p>`)
+
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
