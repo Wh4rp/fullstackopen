3d8b9b5630474a71fda26e7266a091c546b53f96
warp <matiasfernandeztaipe@gmail.com>
2022-10-18 17:31:23 -0300

    4.10: Blog list tests, step3

--- part4/Blog list/tests/blog_api.test.js
+++ part4/Blog list/tests/blog_api.test.js
@@ -12,26 +12,54 @@ beforeEach(async () => {
     await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as JSON', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('get requests', () => {
+    test('blogs are returned as JSON', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('a specific blog is within the returned blogs', async () => {
+        const response = await api.get('/api/blogs')
+        const contents = response.body.map(r => r.title)
+
+        expect(contents).toContain(
+            'Type wars'
+        )
+    })
+
+    test('correct length of blogs returned', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body)
+            .toHaveLength(helper.initialBlogs.length)
+    })
 })
 
-test('a specific blog is within the returned notes', async () => {
-    const response = await api.get('/api/blogs')
-    const contents = response.body.map(r => r.title)
+describe('post requests', () => {
+    const newBlog = {
+        title: 'test title',
+        author: 'me',
+        url: 'https://google.com',
+        likes: 10
+    }
 
-    expect(contents).toContain(
-        'Type wars'
-    )
-})
+    test('post a new blog', async () => {
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-test('correct length of blogs returned', async () => {
-    const response = await api.get('/api/blogs')
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
+        const response = await api.get('/api/blogs')
+        const contents = response.body.map(r => r.title)
+        expect(contents)
+            .toContain(newBlog.title)
+    })
 })
 
 afterAll(() => {
